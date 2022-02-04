6cd826c5d3e518d257e38cbf21216a69cb8238a8
Adrian Alcantara Del fresno <adrfre@alu.iesserpis.org>
2022-02-04 19:21:05 +0100

    archivo java hecho

--- loops1.java
+++ loops1.java
@@ -1,12 +1,17 @@
-
+package com.javatpoint;
+import java.io.*;
 public class loops1 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bufferedReader.readLine().trim());
         int i;
+        int solucion;
         for(i=1;i<=10;i)
         {
+        	solucion=n*x;
+        	System.out.println(n "x "+x+"= "+solucion+" ");
+  
         }
         
         bufferedReader.close();
