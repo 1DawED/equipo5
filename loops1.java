5343169544cfb4cfdb55dc50efdff3e0a315a0ab
Javier Martinez <javgon01@alu.iesserpis.org>
2022-02-04 19:43:18 +0100

    El Java esta hecho

--- loops1.java
+++ loops1.java
@@ -1,4 +1,4 @@
-
+import java.util.*
 public class loops1 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -7,6 +7,7 @@ public class loops1 {
         int i;
         for(i=1;i<=10;i)
         {
+		System.out.println(i + " x "+N+" = "+(N*i));
         }
         
         bufferedReader.close();
